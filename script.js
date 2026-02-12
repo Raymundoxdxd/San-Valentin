191964ccb812d6541d7c8828e4e15d47656cda35
Raymundoxdxd <96947347+Raymundoxdxd@users.noreply.github.com>
2026-02-12 07:45:28 -0600

    Update date in textAnimate function to 2024

--- script.js
+++ script.js
@@ -140,7 +140,7 @@
 
     var textAnimate = eval(Jscex.compile("async", function () {
         var together = new Date();
-        together.setFullYear(2022, 09, 10);
+        together.setFullYear(2024, 11, 10);
         together.setHours(09);
         together.setMinutes(35);
         together.setSeconds(0);
